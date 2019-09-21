f0b5b243edb89e620631756afe7ef008654d94e4
gbd <mail@gabrieldernbach.de>
2019-09-21 17:59:51 +0200

    add comments and wrap function

--- gp_osc/python_implementation/play_interpolated_sequence.py
+++ gp_osc/python_implementation/play_interpolated_sequence.py
@@ -1,42 +1,65 @@
 '''
-Gaussian process with utilities for seemless loops.
-The randomly drawn instances (interval [0,1]) of the process can be considered as wavetables.
+Stochastic Wavetable Synthesis by GP Model Interpolation
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('Qt5Agg')
+matplotlib.use('TkAgg')
 import sounddevice
 
 def gentest(function, start, end, n_samples):
+    '''
+    evaluate function on set of random point in domain [start, end]
+    and rescale domain results to live inside [0, 1]^d
+    '''
     x1 = np.sort(np.random.random(n_samples)) * (end - start) - start
     y1 = function(x1)
     x1s = (x1 + start) / (end - start)
     return x1s, y1
 
 def extrapolate(x1, y1):
+    '''
+    extrapolate supporting points periodically to constrain
+    the model to become periodic in domain [0, 1]
+    '''
     x1e = np.concatenate([x1-1, x1, x1+1])
     y1e = np.concatenate([y1, y1, y1])
     return x1e, y1e
 
 class GP():
     def __init__(self, sigma=0.3, llambda=0.01):
+        '''
+        initialize length scale llambda (effective upper bound of frequncy)
+        and regularization sigma (effectively introduces noise), 
+        but stabalizes possibly ill posed regression tasks (close points impossible to cover)
+        '''
         self.llambda = llambda
         self.sigma = sigma
         return
 
     def kernel(self, a, b):
+        '''
+        squared exponential kernel as used in nadarya watson estimator
+        results in radial basis function as similarity measure
+        '''
         k = np.exp(- (a[:, None] - b[None, :])**2 / (2 * self.sigma ** 2))
         k = k + np.eye(k.shape[0],k.shape[1]) * self.llambda
         return k
 
     def fit(self, x1, y1):
+        '''
+        setup the inverse kernel matrix and save the model supporting points
+        '''
         self.x1 = x1
         self.y1 = y1
         k11 = self.kernel(x1, x1)
         self.k11i = np.linalg.inv(k11)
 
     def predict(self, x2):
+        '''
+        evaluate the model on possibly unseen data points which is 
+        by design a smooth function in x2 and periodic at the ends
+        '''
         k12 = self.kernel(self.x1, x2)
         k22 = self.kernel(x2, x2)
         y2 = k12.T @ self.k11i @ self.y1.T
@@ -44,6 +67,7 @@ class GP():
         return y2, sig2
 
     def fit_predict(self, x1, y1, x2):
+        '''convenience wrapper for joint training and classification'''
         self.fit(x1,y1)
         return self.predict(x2)
 
@@ -57,10 +81,11 @@ class OSC():
         U, S, V = np.linalg.svd(cov)
         self.dewhiten = np.sqrt(S) * U
         
+        # initialize window
         self.window = self.window_builder()
 
     def window_builder(self, relative_margin=0.2):
-        # create window in [0,1]
+        # create window for in out fading of variance
         margin = int(relative_margin * self.num_samples)
         rampin = np.tanh(np.linspace(0,1,margin)) * 1/0.761594156
         rampout = np.tanh(np.linspace(1,0,margin)) * 1/0.761594156
@@ -77,6 +102,21 @@ class OSC():
         instances = self.mean[:,None] + self.window[:,None] * colored_normal
         return instances # wave_len x num_instances
 
+    def generate_interpolated_sequence(self, num_instances, blend_len=96000):
+        table = self.draw_wavetable(num_instances)
+        
+        blend = np.linspace(0, 1, blend_len // 2)
+        ramp = np.append(blend, blend[::-1])
+        X = np.tile(table.T, blend_len // wave_len) * ramp
+        
+        Xa = np.pad(X, ((0,1), (0, 0)))
+        Xb = np.pad(X, ((1,0), (0, 0)))
+
+        X = Xa[:,:blend_len//2] + Xb[:,(blend_len//2):]
+        X = X.ravel()
+        return X
+        
+
 
 if __name__ == '__main__':
 
@@ -90,35 +130,16 @@ if __name__ == '__main__':
     y2, sig2 =  gp.fit_predict(x1e, y1e, x2)
 
     # oscillator
-    wave_len = 1000
-    osc = OSC(gp, wave_len)
-    instances = osc.draw_wavetable(8).T
-
-    table = osc.draw_wavetable(20)
-    
-    blend_len = 96000
-    blend = np.linspace(0, 1, blend_len // 2)
-    ramp = np.append(blend, blend[::-1])
-    X = np.tile(table.T, blend_len // wave_len) * ramp
-
-    
-    Xa = np.pad(X, ((0,1), (0, 0)))
-    Xb = np.pad(X, ((1,0), (0, 0)))
-
-    X = Xa[:,:blend_len//2] + Xb[:,(blend_len//2):]
-    X = X.ravel()
-    
-
-    #X = (X - X.mean()) / (X.max() - X.min())
+    osc = OSC(gp, wave_len=1000)
+    instances = osc.draw_wavetable(num_instance=8)
+    sequence = osc.generate_interpolated_sequence(num_instances=20, blend_len=96000)
     
     def play(X):
         fs = 48000
         sounddevice.play(X,fs)
         sounddevice.wait()
     
-    # X = np.sin(2 * np.pi * np.linspace(0,1,480))
-    # X = np.tile(X[:-1],100)
-    play(X)
+    play(sequence)
 
 
     plt.subplot(211)
